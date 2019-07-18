56e85249332f3d96ee928b3f8a2328494e9373ac
matt-francis051513 <mrthemetribe@gmail.com>
2019-07-19 00:26:27 +0800

    Added set record and clear interval after last record

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,9 @@ var WPAPI = require( 'wpapi' );
 var mysql = require('mysql');
 var wordpress = require( "wordpress" );
 var request = require('request');
+var timer = null;
 
-var resultCount = 0;
+var resultCount = process.env.record || 0;
 const dbSource = mysql.createConnection({
 				host: "localhost",
 				user: "root",
@@ -22,45 +23,38 @@ const dbTransfered = mysql.createConnection({
 
 
 const getWPPost = function(req, res){
-  
-		setInterval(transfer, 1000);
+	timer = setInterval(transfer, 1000);
    };
 
- const transfer = function(req, res){
- 	
-			
-		var query =  `SELECT * from wp_posts limit ${resultCount++} , 1`;
-		dbSource.query(query, function(err, results, fields){
-			if(err)
-				console.log(err);
+const transfer = function(req, res){
+	var query = `SELECT * from wp_posts limit ${resultCount++} , 1`;
+	dbSource.query(query, function(err, results, fields){
+		if(err)
+			console.log(err);
 
-			if(results.length > 0){
-				var result = JSON.stringify(results);
-				result = JSON.parse(result);
+		if(results.length > 0){
+			var result = JSON.stringify(results);
+			result = JSON.parse(result);
 
-				var data = [result[0].ID, result[0].post_title, result[0].post_content, result[0].post_status];
+			var data = [result[0].ID, result[0].post_title, result[0].post_content, result[0].post_status];
 
-				var queryIns = "INSERT INTO posts (post_id, post_title, post_content, post_status) VALUES (?, ?, ?, ?)";
+			var queryIns = "INSERT INTO posts (post_id, post_title, post_content, post_status) VALUES (?, ?, ?, ?)";
 
-				dbTransfered.query(queryIns, data, function(errno, resultTransfer){
-					if(errno)
-						console.log(errno);
-					else
-						console.log({Status: "Success", PostID: result[0].ID});
-				});
-
-
-			}
-		
-		});
+			dbTransfered.query(queryIns, data, function(errno, resultTransfer){
+				if(errno)
+					console.log(errno);
+				else
+					console.log({Status: "Success", PostID: result[0].ID});
+			});
+		}else{
+			console.log({Last_Record: "TRUE", Last_ID: --resultCount});
+			clearInterval(timer);
+		}
+	});
  }
 
 router.get('/', (req, res) => {
-	
 	getWPPost(req, res);
-	
 });
-
-
 
  module.exports = router;
