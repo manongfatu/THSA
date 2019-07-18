be943ae90c68f60293c18c09a94bc453917b47dc
matt-francis051513 <mrthemetribe@gmail.com>
2019-07-18 20:22:01 +0800

    updated js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,70 +3,62 @@ let router = express.Router();
 var WPAPI = require( 'wpapi' );
 var mysql = require('mysql');
 var wordpress = require( "wordpress" );
+var request = require('request');
 
-var getWPPost = function(req, res){
-	var request = require('request');
-    var headers, options;
-
-    // Set the headers
-    headers = {
-        'Content-Type':'application/x-www-form-urlencoded'
-    }
-
-    // Configure the request
-    options = {
-        url: 'http://localhost/cli_testing/wordpress/index.php/wp-json/wp/v2/posts?per_page=100',
-        method: 'GET',
-        headers: headers
-    }
-
-    // Start the request
-    request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-			if(body){
-				console.log('Fetched data')
-			}
-			var con = mysql.createConnection({
+var resultCount = 0;
+const dbSource = mysql.createConnection({
 				host: "localhost",
 				user: "root",
 				password: "",
-				database: 'cli_test'
+				database: 'health'
 			  });
-			var bodys = JSON.parse(body);
-			
-			con.connect(function(err) {
-				if (err) throw err;
-				console.log("Connected!");
-				var results = [[bodys[2].title.rendered]];
-			
-				var sql = "INSERT INTO posts (title) VALUES ?";
-				
-				con.query(sql, [results], function (err, result) {
-				  if (err) throw err;
-				  console.log("Stored 1 data");
-				});
+
+const dbTransfered = mysql.createConnection({
+				host: "localhost",
+				user: "root",
+				password: "",
+				database: 'thsa'
 			  });
-		
-        } else {
-             console.log(error);
-        }
-     });
+
+
+const getWPPost = function(req, res){
+  
+		setInterval(transfer, 1000);
    };
+
+ const transfer = function(req, res){
+ 	
+			
+		var query = SELECT * from wp_posts limit ${resultCount++} , 1;
+		dbSource.query(query, function(err, results, fields){
+			if(err)
+				console.log(err);
+
+			if(results.length > 0){
+				var result = JSON.stringify(results);
+				result = JSON.parse(result);
+
+				var data = [result[0].ID, result[0].post_title, result[0].post_content, result[0].post_status];
+
+				var queryIns = "INSERT INTO posts (post_id, post_title, post_content, post_status) VALUES (?, ?, ?, ?)";
+
+				dbTransfered.query(queryIns, data, function(errno, resultTransfer){
+					if(errno)
+						console.log(errno);
+					else
+						console.log({Status: "Success", PostID: result[0].ID});
+				});
+
+
+			}
+		
+		});
+ }
 
 router.get('/', (req, res) => {
 	
 	getWPPost(req, res);
-	// var wp = new WPAPI({ endpoint: 'http://localhost/cli_testing/wordpress/?rest_route=/'});
-
-    // wp.posts().get(function( err, data ) {
-		
-	//     if ( err ) {
-	//     	console.log({err});
-	//     }else{
-	//     	console.log({ data: data.length });
-    // 		res.send({ data });
-	//     }
-	// });
+	
 });
 
 
