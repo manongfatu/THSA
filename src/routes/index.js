3db777a95a9d0c6a4ce2e2deeba7489c99f28d7b
Francis Albores <mrthemetribe@gmail.com>
2019-08-21 16:04:57 +0800

    To send data to active campaign and to fetch user wp data

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,32 +1,22 @@
 let express = require('express');
 let router = express.Router();
-var WPAPI = require( 'wpapi' );
 var mysql = require('mysql');
-var wordpress = require( "wordpress" );
 var request = require('request-promise');
 var timer = null;
 
 var resultCount = process.env.record || 0;
-const dbTransfered = mysql.createConnection({
-				host: "localhost",
-				user: "developer",
-				password: "aGJXQTJbvaYfvppn",
-				database: 'cli_test'
-			  });
 
 const transfer = async function(req, res){
 	var headers, options;
 	var count = resultCount++;
-    // Set the headers
-    headers = {
-        'Content-Type':'application/x-www-form-urlencoded'
-    }
 
     // Configure the request
     options = {
-        url: 'http://uatvanillavip.wpengine.com/?rest_route=/wp/v2/posts?per_page=1&offset='+count,
+        url: 'http://uatvanillavip.wpengine.com/?rest_route=/wp/v2/users&per_page=1&offset='+count,
         method: 'GET',
-        headers: headers
+        headers: {
+			'Content-Type':'application/x-www-form-urlencoded'
+		}
     }
 	// Start the request
 	try{
@@ -36,29 +26,43 @@ const transfer = async function(req, res){
 			if(timer)
 			if(result.length>0){
           
-			var data =[result[0].id, result[0].title.rendered, result[0].content.rendered, result[0].status];
+				var data =[result[0].id, result[0].title.rendered, result[0].content.rendered, result[0].status];
 
-			var queryIns = "INSERT INTO posts (post_id, post_title, post_content, post_status) VALUES (?, ?, ?, ?)";
-
-			await dbTransfered.query(queryIns, data, function(errno, resultTransfer){
-				if(errno)
-					console.log(errno);
-				else
-					console.log({Status: "Success", PostID: result[0].id, Queue: count+1});
-			});
-
+				let store = await request({
+					url : 'https://thehealthsciencesacademy1562524525.api-us1.com/api/3/contacts',
+					method : 'POST',
+					headers : {
+						'Api-Token' : '9420229f8a7362246d4d1a0cfb584dc0fb137996b708ff7ffea8b97189ab153dc224292c',
+						'Content-Type' : 'application/json'
+					},
+					body : {
+						contact: {
+							email: "johndoe@example.com",
+							firstName: "John",
+							lastName: "Doe",
+							phone: "7223224241"
+						}
+					},
+					json : true
+				})
+				.then(function (res) {
+					// POST succeeded...
+					console.log({Status: "Success", Email: res.email, Queue: count+1});
+				})
+				.catch(function (err) {
+					// POST failed...
+					console.log('Error storing to activecampaign');
+				});
 			}
 			else{
 				clearInterval(timer);
-				console.log({Last_Record: "TRUE"});
-				
+				console.log({Last_Record: "TRUE"});	
 			}    
-	}catch(e){
+	}
+	catch(e){
 		console.log('Fetch failed.');
 	}
-	     
 }
 
 timer = setInterval(transfer, 200);
-
- module.exports = router;
+module.exports = router;
